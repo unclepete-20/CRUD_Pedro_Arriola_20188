593805185238e35e8930828c7fba3a204cafdff7
unclepete-20 <pedroparty310@gmail.com>
2021-05-24 22:52:40 -0600

    Se agregó la base de datos de Firebase para implementar las funciones de CRUD.

--- app/src/main/java/com/example/crud_pedro_arriola_20188/MainActivity.kt
+++ app/src/main/java/com/example/crud_pedro_arriola_20188/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.crud_pedro_arriola_20188
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
